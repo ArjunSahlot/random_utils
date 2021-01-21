94eccd854feb774f88bbea4cb2565243e234d4b9
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-21 00:11:55 -0800

    numeric funcs

--- random_utils/numeric/__init__.py
+++ random_utils/numeric/__init__.py
@@ -1,6 +1,20 @@
 def is_prime(num):
-    return
+    if num <= 1:
+        return False
+
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
 
 
 def find_factors(num):
-    return
+    factors = []
+    for i in range(1, int(num**0.5)+1):
+        if num % i == 0:
+            if i not in factors:
+                factors.append(i)
+            if num//i not in factors:
+                factors.append(num//i)
+
+    return factors
