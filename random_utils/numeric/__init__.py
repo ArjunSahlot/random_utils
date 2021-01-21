dd933ac9e4c4fd786157cc591b709afd0b4db350
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-21 00:30:40 -0800

    improved algs

--- random_utils/numeric/__init__.py
+++ random_utils/numeric/__init__.py
@@ -1,20 +1,18 @@
-def is_prime(num):
-    if num <= 1:
-        return False
+from functools import reduce
 
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
-            return False
-    return True
+
+def is_prime(num):
+    if num & 1 == 0:
+        return True
+    d = 3
+    while d ** 2 <= num:
+        if num % d == 0:
+            return True
+        d += 2
+    return False
 
 
 def find_factors(num):
-    factors = []
-    for i in range(1, int(num**0.5)+1):
-        if num % i == 0:
-            if i not in factors:
-                factors.append(i)
-            if num//i not in factors:
-                factors.append(num//i)
-
-    return factors
+    step = 2 if num % 2 else 1
+    factors = ([i, num // i] for i in range(1, int(num ** 0.5) + 1, step) if num % i == 0))
+    return set(reduce(list.__add__, factors))
