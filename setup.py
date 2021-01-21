05cf5f16818877d8a3b9cdcb38fba4f23534c752
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-21 00:32:25 -0800

    Version0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_utils",
-    version="0.0.8",
+    version="0.0.9",
     author="Arjun Sahlot",
     author_email="iarjun.sahlot@gmail.com",
     description="Module with various useful utilities.",
