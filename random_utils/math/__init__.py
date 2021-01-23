ae8f89d933298f6d88c64fe612f01896769c9c36
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-22 18:18:59 -0800

    find_factors fix

--- random_utils/math/__init__.py
+++ random_utils/math/__init__.py
@@ -20,4 +20,4 @@ def find_factors(num, sort=True):
     step = 2 if num % 2 else 1
     factors = ([i, num // i] for i in range(1, int(num ** 0.5) + 1, step) if num % i == 0)
     rv = set(reduce(list.__add__, factors))
-    return sorted(rv) if sort else rv
+    return sorted(rv) if sort else list(rv)
