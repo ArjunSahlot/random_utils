b9c4d8128a1acb8619da15df62cadce54cfc13a8
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-22 18:11:41 -0800

    fixed prime

--- random_utils/math/__init__.py
+++ random_utils/math/__init__.py
@@ -3,14 +3,17 @@ from .vector import Vector
 
 
 def is_prime(num):
-    if num & 1 == 0:
-        return True
-    d = 3
-    while d ** 2 <= num:
-        if num % d == 0:
-            return True
-        d += 2
-    return False
+    if num == 2 or num == 3: return True
+    if num < 2 or num % 2 == 0: return False
+    if num < 9: return True
+    if num % 3 == 0: return False
+    r = int(num**0.5)
+    f = 5
+    while f <= r:
+        if num % f == 0: return False
+        if num % (f+2) == 0: return False
+        f += 6
+    return True
 
 
 def find_factors(num, sort=True):
