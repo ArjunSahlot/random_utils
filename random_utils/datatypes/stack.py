3637ba60274caeb251235eb6bf12b2e30c614706
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-23 19:00:07 -0800

    finsihed stack

---
+++ random_utils/datatypes/stack.py
@@ -0,0 +1,33 @@
+class Stack:
+    def __init__(self, *args):
+        self.arr = list(args)
+
+    def push(self, element):
+        self.arr.append(element)
+
+    def pop(self):
+        self.arr.pop(-1)
+
+    def get_list(self):
+        return self.arr
+
+    def __iter__(self):
+        return iter(self.arr)
+
+    def __add__(self, lst):
+        self.arr.extend(lst)
+
+    def __sub__(self, num):
+        for i in range(len(self.arr)):
+            self.arr[i] -= num
+
+    def __mul__(self, num, individually=True):
+        if individually:
+            for i in range(len(self.arr)):
+                self.arr[i] *= num
+        else:
+            self.arr *= num
+
+    def __div__(self, num):
+        for i in range(len(self.arr)):
+            self.arr[i] /= num
