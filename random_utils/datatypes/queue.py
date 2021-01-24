5b834dcf64d6d0a1f7e968d42f4fa67fc858b1a1
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-01-23 19:04:48 -0800

    size

--- random_utils/datatypes/queue.py
+++ random_utils/datatypes/queue.py
@@ -2,6 +2,9 @@ class Queue:
     def __init__(self, *args):
         self.arr = list(args)
 
+    def size(self):
+        return len(self.arr)
+
     def push(self, element):
         self.arr.append(element)
 
